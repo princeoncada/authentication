7aa902088e7f3cd521550bb3975a425607c51619
Prince Oncada <prince.oncada@gmail.com>
2023-02-25 12:25:53 +0800

    4th Commit / Hashing Passwords

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
+// const encrypt = require('mongoose-encryption'); - used for database encryption
 
 const app = express();
 
@@ -20,8 +21,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-// The key value in { secret: secret } is the default value. Always use secret key value.
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// The key value in { secret: secret } is the default value. Always use secret key value. - used for database encryption
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] }); - used for database encryption
 
 const User = new mongoose.model('User', userSchema);
 
@@ -35,7 +36,7 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
     const username = req.body.username
-    const password = req.body.password
+    const password = md5(req.body.password)
 
     User.findOne({ email: username }, function (err, foundUser) {
         if(err) {
@@ -57,7 +58,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save(err => {
